7c27e242c7b5b73b26b1f8edbc8cf782914c31a2
Artemkuz <artemkuz1083@gmail.com>
2024-11-20 13:37:08 +0500

    fix repository

---
+++ AlgorythmsLab-3/Program.cs
@@ -0,0 +1,41 @@
+﻿using AlgorythmsLab_3.Utilities;
+using OfficeOpenXml;
+
+class Program
+{
+    public static void Main()
+    {
+        ExcelPackage.LicenseContext = LicenseContext.Commercial;
+
+        MenuManager.ShowMainMenu();
+
+        while (true)
+        {
+            int choice = MenuManager.GetMenuChoice();
+
+            switch (choice)
+            {
+                case 1:
+                    Console.Clear();
+                    Tasks.RunStackTasks();
+                    break;
+                case 2:
+                    Console.Clear();
+                    Tasks.RunQueueTasks();
+                    break;
+                case 3:
+                    Console.Clear();
+                    Tasks.RunDynamicStructuresTasks();
+                    break;
+                case 4:
+                    Console.Clear();
+                    Tasks.RunListTasks();
+                    break;
+                default:
+                    Console.WriteLine("Error: Введите номер от 1 до 4!");
+                    break;
+            }
+        }
+       
+    }
+}
---
+++ AlgorythmsLab-3/Utilities/MenuManager.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace AlgorythmsLab_3.Utilities
+{
+    public static class MenuManager
+    {
+        public static void ShowMainMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Выберите структуру данных:");
+            Console.WriteLine("1. Стек");
+            Console.WriteLine("2. Очередь");
+            Console.WriteLine("3. Динамические структуры");
+            Console.WriteLine("4. Лист");
+        }
+
+        public static void ReturnToMainMenu(string previousMethod)
+        {
+            Console.WriteLine("Введите 1, чтобы вернуться назад.");
+            Console.WriteLine("Введите 2, чтобы ввернуться в главное меню.");
+            Console.WriteLine("Введите 3, чтобы завершить программу.");
+            while (true)
+            {
+                int choice = GetMenuChoice();
+                switch (choice)
+                {
+                    case 1:
+                        switch (previousMethod)
+                        {
+                            case "Stack":
+                                Tasks.RunStackTasks();
+                                break;
+                            case "Queue":
+                                Tasks.RunQueueTasks();
+                                break;
+                            case "DinamicStructure":
+                                Tasks.RunDynamicStructuresTasks();
+                                break;
+                            case "List":
+                                Tasks.RunListTasks();
+                                break;
+                            // Добавьте другие ваши кейсы по мере необходимости
+                            default:
+                                Program.Main();
+                                break;
+                        }
+                        return;
+                    case 2:
+                        Program.Main();
+                        break;
+                    case 3:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Введите номер от 1 до 3!");
+                        break;
+                }
+            }
+        }
+
+
+        public static int GetMenuChoice()
+        {
+            if (int.TryParse(Console.ReadLine(), out int choice))
+            {
+                return choice;
+            }
+            return -1;
+        }
+    }
+}
